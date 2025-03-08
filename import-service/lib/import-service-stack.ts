062a648c9765805c9219cdfd12b966debffe67e7
MaksimNK <maksmozir@gmail.com>
2025-03-08 19:03:36 +0300

    fead: add gateway

---
+++ import-service/lib/import-service-stack.ts
@@ -0,0 +1,76 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as path from 'path';
+
+export class ImportServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = s3.Bucket.fromBucketName(this, 'ImportBucket', 'import-service-bucket-ts');
+
+    const importProductsFileLambda = new NodejsFunction(this, 'ImportProductsFileFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'importProductsFile',
+      entry: path.join(__dirname, '../lambda/importProductsFile.ts'),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        REGION: this.region,
+      },
+      bundling: {
+        externalModules: [],
+        minify: true,
+        sourceMap: true,
+      },
+    });
+
+    const importFileParserFunction = new NodejsFunction(this, 'ImportFileParserFunction', {
+      functionName: 'importFileParser',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'importFileParser',
+      entry: path.join(__dirname, '../lambda/importFileParser.ts'),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        REGION: this.region,
+      },
+      bundling: {
+        externalModules: [],
+        minify: true,
+        sourceMap: true,
+      },
+    });
+
+    bucket.grantReadWrite(importProductsFileLambda);
+    bucket.grantRead(importFileParserFunction);
+
+    const api = new apigateway.RestApi(this, 'ImportApi', {
+      restApiName: 'Import Service',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    const importResource = api.root.addResource('import');
+    importResource.addMethod('GET', new apigateway.LambdaIntegration(importProductsFileLambda), {
+      requestParameters: {
+        'method.request.querystring.name': true,
+      },
+    });
+
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.LambdaDestination(importFileParserFunction),
+      { prefix: 'uploaded/' }
+    );
+
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
+      description: 'The URL of the Import API',
+    });
+  }
+}
