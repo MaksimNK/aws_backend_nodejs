9ac6997f1eea9fd3a5e9af0fce1570e68e5fa7e1
MaksimNK <maksmozir@gmail.com>
2025-03-15 20:38:03 +0300

    feat: add catalogBatchProcess

--- product-service/lib/backend_aws_2025-stack.ts
+++ product-service/lib/backend_aws_2025-stack.ts
@@ -5,10 +5,81 @@ import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
 import { Construct } from 'constructs';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { SubscriptionFilter, Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export class BackendAws2025Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const catalogItemQueue = new Queue(this, 'CatalogItemsQueue', {
+      queueName: 'catalogItemsQueue',
+      visibilityTimeout: cdk.Duration.seconds(30),
+    });
+
+    const createProductTopic = new Topic(this, 'CreateProductTopic', {
+      topicName: 'createProductTopic'
+    });
+
+    createProductTopic.addSubscription(
+      new EmailSubscription(process.env.EMAIL_HIGH_PRICE!, {
+        filterPolicy: {
+          price: SubscriptionFilter.numericFilter({
+            greaterThanOrEqualTo: 100,
+          })
+        }
+      })
+    )
+
+    createProductTopic.addSubscription(
+      new EmailSubscription(process.env.EMAIL_LOW_PRICE!, {
+        filterPolicy: {
+          price: SubscriptionFilter.numericFilter({
+            lessThan: 100,
+          })
+        }
+      })
+    )
+
+
+    const catalogBatchProcess = new nodejsLambda.NodejsFunction(this, 'CatalogBatchProcess', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: path.join(__dirname, 'lambdas/catalogBatchProcess.ts'),
+      timeout: cdk.Duration.seconds(60),
+      memorySize: 512,
+      bundling: {
+        minify: true,
+        sourceMap: true,
+      },
+      environment: {
+        PRODUCTS_TABLE: "products",
+        SNS_TOPIC_ARN: createProductTopic.topicArn,
+      }
+    });
+    catalogBatchProcess.addEventSource(
+      new SqsEventSource(catalogItemQueue, {
+        batchSize: 5,
+      })
+    )
+
+    createProductTopic.grantPublish(catalogBatchProcess);
+
+    catalogBatchProcess.addToRolePolicy(new iam.PolicyStatement({
+      actions: [
+        "dynamodb:PutItem",
+        "dynamodb:TransactWriteItems"
+      ],
+      resources: [
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/products`,
+        `arn:aws:dynamodb:us-east-1:${this.account}:table/stocks`
+      ]
+    }));
 
     const getProductsListLambda = new nodejsLambda.NodejsFunction(this, 'GetProductsListHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
